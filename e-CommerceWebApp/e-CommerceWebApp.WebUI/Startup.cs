cf22370834eaf552ed086d5c23fb605cef0210e5
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2018-02-10 15:35:10 -0800

    Initial Solution Commit

---
+++ e-CommerceWebApp/e-CommerceWebApp.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_CommerceWebApp.WebUI.Startup))]
+namespace e_CommerceWebApp.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
